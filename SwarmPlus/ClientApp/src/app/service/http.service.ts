5b6f0762ea424fc8180ce479841b6a6480d3e91d
SuyamaDaichi <symdit@gmail.com>
2019-07-19 06:28:23 +0900

    httpService作成

---
+++ SwarmPlus/ClientApp/src/app/service/http.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+  constructor(private httpClient: HttpClient) { }
+
+  /**
+   * login
+   */
+  public login() {
+    return this.httpClient.get(environment.backEndApi + '/login');
+  }
+}
