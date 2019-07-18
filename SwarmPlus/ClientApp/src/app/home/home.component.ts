b41d07138a778ff003dc88ca86437313f61e43f7
SuyamaDaichi <symdit@gmail.com>
2019-07-19 06:50:46 +0900

    バックエンドにログインリクエストするひな型作成

--- SwarmPlus/ClientApp/src/app/home/home.component.ts
+++ SwarmPlus/ClientApp/src/app/home/home.component.ts
@@ -1,21 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { HttpService } from '../service/http.service';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit {
+  /** バックエンドAPI */
+  backendAPI = environment.backEndApi;
+
   items: AngularFirestoreCollection<string>;
 
-  constructor(public db: AngularFirestore) {
+  constructor(public db: AngularFirestore, private httpService: HttpService) {
     this.items = db.collection('users');
   }
 
 
   ngOnInit() {
-    this.items.doc('eb63c6c5-9591-4436-910c-9e86e6ad17d7').get().subscribe(data => {
-      console.log(data.data());
-    });
   }
 }
