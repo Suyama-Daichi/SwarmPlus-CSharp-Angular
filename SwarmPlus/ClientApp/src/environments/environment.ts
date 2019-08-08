63f2748f4f17e4eb009923ee692d6cc5f6873784
SuyamaDaichi <symdit@gmail.com>
2019-08-08 23:21:05 +0900

    environment.tsをリポジトリに追加

---
+++ SwarmPlus/ClientApp/src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: 'AIzaSyBDDSgopNCKorq75jcyksNSYXFOZ_gwZqY',
+        authDomain: 'swarmplus.firebaseapp.com',
+        databaseURL: 'https://swarmplus.firebaseio.com',
+        projectId: 'swarmplus',
+        storageBucket: 'swarmplus.appspot.com',
+        messagingSenderId: '467829541317',
+        appId: '1:467829541317:web:b043b5717f36d61f'
+    },
+    backEndApi: 'http://localhost:49947/api'
+};
