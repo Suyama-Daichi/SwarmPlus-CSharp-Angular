0a893b45546e90d49b5f72bc74bb62c8578667f9
SuyamaDaichi <symdit@gmail.com>
2019-08-21 23:02:02 +0900

    月間250チェックイン以上している人のための対応

--- SwarmPlus/Service/FoursquareAPI.cs
+++ SwarmPlus/Service/FoursquareAPI.cs
@@ -37,13 +37,35 @@ namespace SwarmPlus.Service
         /// <param name="afterTimestamp">取得する期間(始まり)</param>
         /// <param name="beforeTimestamp">取得する期間(終わり)</param>
         /// <returns></returns>
-        public async Task<UsersCheckins> GetUsersCheckinsAsync(string uuid ,int afterTimestamp, int beforeTimestamp)
+        public async Task<UsersCheckins> GetUsersCheckinsAsync(string uuid, int afterTimestamp, int beforeTimestamp)
         {
             string accesstoken = _db.User.FirstOrDefault(f => f.UserID == uuid).AccessToken;
             var response = await Client.GetAsync(
                 $"users/self/checkins?oauth_token={accesstoken}&v=20180815&limit=250&afterTimestamp={afterTimestamp}&beforeTimestamp={beforeTimestamp}");
             var result = await response.Content.ReadAsStringAsync();
             var deserialisedResult = JsonConvert.DeserializeObject<UsersCheckins>(result);
+            // 250チェックイン/月する場合の処理
+            if (deserialisedResult.response.checkins.items.Length == 250)
+            {
+                HttpResponseMessage moreResponse = await Client.GetAsync(
+                $"users/self/checkins?oauth_token={accesstoken}&v=20180815&limit=250&afterTimestamp={afterTimestamp}&beforeTimestamp={deserialisedResult.response.checkins.items.Last().createdAt}");
+                string moreResult = await moreResponse.Content.ReadAsStringAsync();
+                UsersCheckins moreDeserialisedResult = JsonConvert.DeserializeObject<UsersCheckins>(moreResult);
+                //deserialisedResult.response.checkins.items.Concat(moreDeserialisedResult.response.checkins.items);
+                return new UsersCheckins
+                {
+                    meta = deserialisedResult.meta,
+                    notifications = deserialisedResult.notifications,
+                    response = new Response
+                    {
+                        checkins = new Checkins
+                        {
+                            count = deserialisedResult.response.checkins.count,
+                            items = deserialisedResult.response.checkins.items.Concat(moreDeserialisedResult.response.checkins.items).ToArray()
+                        }
+                    }
+                };
+            }
             return deserialisedResult;
         }
     }
