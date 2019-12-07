5b466a0ece6868aede8fb4eae6ad75ac1c45d1c4
SuyamaDaichi <symdit@gmail.com>
2019-12-07 09:45:16 +0900

    ログアウト処理

--- SwarmPlus/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ SwarmPlus/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
   styleUrls: ['./nav-menu.component.scss']
 })
-export class NavMenuComponent {
+export class NavMenuComponent implements OnInit {
+
+  constructor(private router: Router) { }
   isExpanded = false;
+
+  ngOnInit() { }
 
   collapse() {
     this.isExpanded = false;
@@ -14,5 +19,10 @@ export class NavMenuComponent {
 
   toggle() {
     this.isExpanded = !this.isExpanded;
+  }
+
+  logOut() {
+    localStorage.removeItem('token');
+    this.router.navigateByUrl('');
   }
 }
