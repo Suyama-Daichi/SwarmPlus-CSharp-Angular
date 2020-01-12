679c1cff9960dd0a0897072cd86ba07266936bad
SuyamaDaichi <symdit@gmail.com>
2020-01-12 16:38:06 +0900

    ログインしている場合、Home画面ではなくmonthに遷移させる

--- SwarmPlus/ClientApp/src/app/home/home.component.ts
+++ SwarmPlus/ClientApp/src/app/home/home.component.ts
@@ -1,17 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import { environment } from '../../environments/environment';
+import { Router } from '@angular/router';
+import { HttpService } from '../service/http.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit {
-  /** 認可URL */
-  authenticateURL = environment.authenticateURL;
-  /** リダイレクトURL */
-  redirectUrl = environment.redirectUrl;
+  constructor(
+    private router: Router,
+    private httpService: HttpService
+    ) { }
 
-  constructor() { }
-
-  ngOnInit() {}
+  ngOnInit() {
+    this.httpService.VerifyAccessToken(localStorage.getItem('token')).subscribe(
+      response => {
+        if (response.meta.code !== 401) {
+          this.router.navigateByUrl('/month');
+        }
+      }
+    );
+  }
 }
