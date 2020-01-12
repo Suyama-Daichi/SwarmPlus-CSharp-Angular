5c6014d90b82d314437b5c93289d4e027acec7ab
SuyamaDaichi <symdit@gmail.com>
2020-01-12 18:45:02 +0900

    ビルドエラー修正

--- SwarmPlus/ClientApp/src/app/home/home.component.ts
+++ SwarmPlus/ClientApp/src/app/home/home.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpService } from '../service/http.service';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit {
+    /** 認可URL */
+    authenticateURL = environment.authenticateURL;
+    /** リダイレクトURL */
+    redirectUrl = environment.redirectUrl;
   constructor(
     private router: Router,
     private httpService: HttpService
