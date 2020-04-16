cbd1292deaaf4a9a2f57e3913cd7a1f83a6d123b
SuyamaDaichi <symdit@gmail.com>
2020-04-16 20:50:18 +0900

    バックエンドURL変更

--- SwarmPlus/ClientApp/src/environments/environment.prod.ts
+++ SwarmPlus/ClientApp/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  backEndApi: 'https://swarmplusdev.azurewebsites.net/api',
+  backEndApi: 'https://swarmplus20200416193938.azurewebsites.net/api',
   authenticateURL: 'https://foursquare.com/oauth2/authenticate?client_id=1DSKDVJVFM3FHGAREG1LUY2NWJVX452M0UVDO1KAAIVCMVZ5&response_type=code',
   redirectUrl: 'https://swarmplus.net/login'
 };
