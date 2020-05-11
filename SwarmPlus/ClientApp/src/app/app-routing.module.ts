c19d40fafc6d9873747aad47a889653b967a4f88
SuyamaDaichi <symdit@gmail.com>
2020-05-11 21:40:52 +0900

    ルーティングの順序修正

--- SwarmPlus/ClientApp/src/app/app-routing.module.ts
+++ SwarmPlus/ClientApp/src/app/app-routing.module.ts
@@ -8,12 +8,12 @@ import { MainComponent } from './CommonComponent/main/main.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: '**', redirectTo: 'month' },
   { path: 'month', component: MainComponent, canActivate: [AuthGuard] },
   { path: 'month/:year/:month', component: MainComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'day/:year/:month/:date', component: DayViewComponent, canActivate: [AuthGuard] },
-  { path: 'day', component: DayViewComponent, canActivate: [AuthGuard] }
+  { path: 'day', component: DayViewComponent, canActivate: [AuthGuard] },
+  { path: '**', redirectTo: 'month' }
 ];
 
 @NgModule({
